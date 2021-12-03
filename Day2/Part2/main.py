a33a95d6e71f3b10887840bea0509c362f2471b4
pc592 <pc592@cornell.edu>
2021-12-03 00:42:39 -0500

    Day 2, Part 2 complete!

---
+++ Day2/Part2/main.py
@@ -0,0 +1,55 @@
+from typing import List;
+from dataclasses import dataclass;
+
+fileName = './Day2/input.txt';
+
+def parseFileIntoArray(fileName) -> List[str]:
+  # reads into an array
+  with open(fileName) as f:
+    lines = f.readlines()
+  
+  # sanitize (using list comprehension)
+  sanitizedLines = [line.strip() for line in lines];
+
+  return sanitizedLines;
+
+@dataclass
+class Position:
+  x: int = 0;
+  y: int = 0;
+  aim: int = 0;
+
+@dataclass
+class Instruction:
+  direction: str;
+  unit: str;
+
+def parseInput(input: str) -> Instruction:
+  splitInput = input.split();
+  return Instruction(splitInput[0], splitInput[1]);
+
+def getFinalPosition(inputs: List[str]) -> Position:
+  position = Position();
+
+  for input in inputs:
+    instruction = parseInput(input);
+    match instruction.direction:
+      case 'forward':
+        position.x += int(instruction.unit);
+        position.y += position.aim*int(instruction.unit);
+      case 'down':
+        position.aim += int(instruction.unit);
+      case 'up':
+        position.aim -= int(instruction.unit);
+
+  return position;
+
+def main():
+  # inputs = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2'];
+  inputs = parseFileIntoArray(fileName);
+  finalPostion = getFinalPosition(inputs);
+  result = finalPostion.x * finalPostion.y;
+  print(f'{finalPostion=}');
+  print(f'multiplied: {result}');
+
+main();
